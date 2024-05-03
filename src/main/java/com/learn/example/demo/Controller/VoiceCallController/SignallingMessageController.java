47c92bae9960c45aa638012f83de471543633b38
Heni Mehta <88430391+heni2021@users.noreply.github.com>
2024-05-03 11:44:34 +0530

    Committing changes for web socket video call

--- src/main/java/com/learn/example/demo/Controller/VoiceCallController/SignallingMessageController.java
+++ src/main/java/com/learn/example/demo/Controller/VoiceCallController/SignallingMessageController.java
@@ -14,6 +14,6 @@ public class SignallingMessageController {
 
     @MessageMapping("/send")
     public void sendSignallingMessage(@Payload SignallingMessagePOJO message){
-        messagingTemplate.convertAndSendToUser(message.getReceiver(), "/topic/signaling", message);
+        messagingTemplate.convertAndSendToUser(message.getReceiver(), "/topic/signaling/"+message.getReceiver(), message);
     }
 }
